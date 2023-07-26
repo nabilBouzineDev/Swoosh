d92484c18335cc9c0e5b57940eb84b3872f565d7
Nabil Bouzine <nabil@nabilBouzineDev>
2023-07-26 10:51:15 +0100

    league select screen

---
+++ app/src/main/java/com/nabilbdev/swoosh/LeagueActivity.kt
@@ -0,0 +1,11 @@
+package com.nabilbdev.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LeagueActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_league)
+    }
+}
