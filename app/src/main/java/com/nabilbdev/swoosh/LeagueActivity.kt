7a83f74aa37f7604033a7cd5ff8c4335f6811373
Nabil Bouzine <nabil@nabilBouzineDev>
2023-07-26 20:14:56 +0100

    skill select

--- app/src/main/java/com/nabilbdev/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/nabilbdev/swoosh/LeagueActivity.kt
@@ -1,11 +1,19 @@
 package com.nabilbdev.swoosh
 
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 
 class LeagueActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    // same like click listen in onCreate
+    fun leagueNextClicked(view: View) {
+        val skillIntent = Intent(this, SkillActivity::class.java)
+        startActivity(skillIntent)
     }
 }
---
+++ app/src/main/java/com/nabilbdev/swoosh/SkillActivity.kt
@@ -0,0 +1,11 @@
+package com.nabilbdev.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+}
