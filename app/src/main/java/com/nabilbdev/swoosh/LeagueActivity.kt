f0923c45108371cdc0538106bdbc8a25fef7ec85
Nabil Bouzine <nabil@nabilBouzineDev>
2023-07-26 18:14:56 +0100

    Base Activity

--- app/src/main/java/com/nabilbdev/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/nabilbdev/swoosh/LeagueActivity.kt
@@ -3,7 +3,7 @@ package com.nabilbdev.swoosh
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
--- app/src/main/java/com/nabilbdev/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/nabilbdev/swoosh/WelcomeActivity.kt
@@ -5,15 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
         val getStarted = findViewById<Button>(R.id.startBtn)
-            getStarted.setOnClickListener {
-                val leagueIntent = Intent(this, LeagueActivity::class.java)
-                startActivity(leagueIntent)
-            }
+        getStarted.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
